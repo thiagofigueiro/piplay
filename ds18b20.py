dfb21cb3007e53943b94ab0b25107ca0c9b76a77
Thiago Figueiró <thiagocsf@gmail.com>
2017-12-03 19:01:12 +1100

    Write data to carbon

--- ds18b20.py
+++ ds18b20.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import datetime
+import graphitesend
+import statsd
 import time
 import RPi.GPIO as GPIO
 
+from graphitesend import GraphiteSendException
 
 SENSORS = {
     'outside': '/sys/bus/w1/devices/28-041661c3b1ff/w1_slave',
     'inside': '/sys/bus/w1/devices/28-031661b1c7ff/w1_slave',
 }
+FILE_OUT = 'temperature.csv'
+
+try:
+    graphitesend.init(graphite_server='localhost')
+    GRAPHITE_OUT = True
+except GraphiteSendException:
+    GRAPHITE_OUT = False
+    print('Could not find Carbon. Not sending data to graphite.')
+
 
 GPIO.setmode(GPIO.BCM)
 
 
 def print_temperatures(sensors):
-    print('{}'.format(datetime.datetime.now().isoformat()))
+    now = datetime.datetime.now().isoformat()
+    print('{}'.format(now))
     for name, sensor_path in sensors.items():
         with open(sensor_path, 'r') as sensor_fh:
             data = sensor_fh.readlines()
@@ -30,6 +43,14 @@ def print_temperatures(sensors):
 
         raw_temperature = int(line.split('=')[1])
         temperature = raw_temperature / 1000
+
+        if GRAPHITE_OUT:
+            graphitesend.send('temp_' + name, temperature)
+
+        if FILE_OUT:
+            with open(FILE_OUT, 'a') as out_fh:
+                out_fh.write('{now},{name}={temperature}\n'.format(**locals()))
+
         print('Sensor {name}: {temperature}°C'.format(**locals()))
 
 
