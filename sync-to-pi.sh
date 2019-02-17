05c0df1103ed2de6ff95aabd52b203e7b4554dc3
Thiago Figueiró <thiagocsf@gmail.com>
2019-02-17 12:02:04 +1100

    temperature sanity check

--- sync-to-pi.sh
+++ sync-to-pi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+#fswatch -o . | xargs -n1 -I{} rsync -av ./ 192.168.1.4:pi/
 fswatch -o . | xargs -n1 -I{} rsync -av ./ 192.168.1.15:pi/
+#fswatch -o . | xargs -n1 -I{} bash -c 'rsync -av ./ 192.168.1.15:pi/; rsync -av ./ 192.168.1.4:pi/'
 
